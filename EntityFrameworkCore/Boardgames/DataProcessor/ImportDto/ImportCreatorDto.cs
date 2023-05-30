888690c259c7c7ef6ddc3cafa6b65f086a50c639
Stoyan Karaperev <104765973+StoyanKaraperev@users.noreply.github.com>
2023-05-30 11:30:48 +0300

    Add files via upload

---
+++ EntityFrameworkCore/Boardgames/DataProcessor/ImportDto/ImportCreatorDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Boardgames.DataProcessor.ImportDto;
+
+using Boardgames.Common;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+[XmlType("Creator")]
+public class ImportCreatorDto
+{
+    [XmlElement("FirstName")]
+    [Required]
+    [MinLength(ValidationConstants.CreatorFirstNameMinLength)]
+    [MaxLength(ValidationConstants.CreatorFirstNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [XmlElement("LastName")]
+    [Required]
+    [MinLength(ValidationConstants.CreatorLastNameMinLength)]
+    [MaxLength(ValidationConstants.CreatorLastNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    [XmlArray("Boardgames")] 
+    public ImportBoardgame[] ImportsBoardgames { get; set; } = null!; 
+
+}
