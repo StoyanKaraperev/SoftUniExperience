e533f293a2fb3d5e2d82d54b4d8c1d65944a9844
Stoyan Karaperev <104765973+StoyanKaraperev@users.noreply.github.com>
2023-05-30 11:32:24 +0300

    Add files via upload

---
+++ EntityFrameworkCore/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,25 @@
+﻿namespace Trucks.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using Trucks.Common;
+
+public class Despatcher
+{
+    public Despatcher()
+    {
+        this.Trucks = new HashSet<Truck>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstants.DespatcherNameMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public string Position { get; set; } = null!;
+
+
+    public virtual ICollection<Truck> Trucks { get; set; } = null!;
+}
---
+++ EntityFrameworkCore/Trucks/DataProcessor/ImportDto/ImportTruckDto.cs
@@ -0,0 +1,40 @@
+﻿namespace Trucks.DataProcessor.ImportDto;
+
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Common;
+
+[XmlType("Truck")]
+public class ImportTruckDto
+{
+    [XmlElement("RegistrationNumber")]
+    [Required]
+    [MinLength(ValidationConstants.TruckRegistrationNumberMinLength)]
+    [MaxLength(ValidationConstants.TruckRegistrationNumberMaxLength)]
+    [RegularExpression(ValidationConstants.TruckRegistrationNumberRegex)]
+    public string RegistrationNumber { get; set; } = null!;
+
+    [XmlElement("VinNumber")]
+    [Required]
+    [MinLength(ValidationConstants.TruckVinNumberMinLength)]
+    [MaxLength(ValidationConstants.TruckVinNumberMaxLength)]
+    public string VinNumber { get; set; } = null!;
+
+    [XmlElement("TankCapacity")]
+    [Range(ValidationConstants.TruckTankCapacityMinLength, ValidationConstants.TruckTankCapacityMaxLength)]
+    public int TankCapacity { get; set; }
+
+    [XmlElement("CargoCapacity")]
+    [Range(ValidationConstants.TruckCargoCapacityMinLength, ValidationConstants.TruckCargoCapacityMaxLength)]
+    public int CargoCapacity { get; set; }
+
+    [XmlElement("CategoryType")]
+    [Required]
+    [Range(ValidationConstants.TruckCategoryTypeMinLength, ValidationConstants.TruckCategoryTypeMaxLength)]
+    public int CategoryType { get; set; }
+
+    [XmlElement("MakeType")]
+    [Required]
+    [Range(ValidationConstants.TruckMakeTypeMinLength, ValidationConstants.TruckMakeTypeMaxLength)]
+    public int MakeType { get; set; }
+}
