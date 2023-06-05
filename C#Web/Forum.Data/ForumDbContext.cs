ce4395c8eb87df8c49b41b1587966ceeb197f7a5
Stoyan Karaperev <104765973+StoyanKaraperev@users.noreply.github.com>
2023-06-05 16:06:17 +0300

    Add files via upload

---
+++ C#Web/Forum.Data/ForumDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Forum.Data;
+
+using Forum.Data.Configuration;
+using Forum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ForumDbContext : DbContext
+{
+    public ForumDbContext(DbContextOptions<ForumDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PostEntityConfiguration()); 
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ C#Web/Forum.Services/Interfaces/IPostServices.cs
@@ -0,0 +1,16 @@
+﻿namespace Forum.Services.Interfaces;
+
+using Forum.ViewModels.Posts;
+
+public interface IPostServices
+{
+    Task<IEnumerable<PostListViewModel>> ListAllAsync();
+
+    Task AddPostAsync(PostAddForumModel postViewModel);
+
+    Task<PostAddForumModel> GetForEditOrDeleteByIdAsync(string id);
+
+    Task EditByIdAsync(string id, PostAddForumModel postEditedModel);
+
+    Task DeleteByIdAsync(string id);
+}
