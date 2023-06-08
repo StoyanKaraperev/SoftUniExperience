fdeb65c4f00178cc38902077c83e3abbffd5d79a
Stoyan Karaperev <104765973+StoyanKaraperev@users.noreply.github.com>
2023-06-08 18:22:34 +0300

    Add files via upload

---
+++ C#Web/Watchlist/Watchlist/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace Watchlist.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        if (User?.Identity?.IsAuthenticated ?? false)
+        {
+            return RedirectToAction("All", "Movies");
+        }
+
+        return View();
+    }
+}
---
+++ C#Web/Watchlist/Watchlist/Controllers/MoviesController.cs
@@ -0,0 +1,94 @@
+﻿namespace Watchlist.Web.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Security.Claims;
+using Watchlist.Services.Interfaces;
+using Watchlist.View.Models.Movies;
+
+[Authorize]
+public class MoviesController : Controller
+{
+    private readonly IMovieService movieService;
+
+    public MoviesController(IMovieService _movieService)
+    {
+        this.movieService = _movieService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> All()
+    {
+        var model = await movieService.GetAllAsync();
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        var model = new MoviesAddViewModel()
+        {
+            Genres = await movieService.GetGenresAsync()
+        };
+        
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(MoviesAddViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        try
+        {
+            await movieService.AddMovieAsync(model); 
+            return RedirectToAction(nameof(All));
+        }
+        catch (Exception)
+        {
+            ModelState.AddModelError("", "Something wrong");
+            return View(model);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddToCollection(int movieId)
+    {
+        try
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            await movieService.AddMovieToCollectionAsync(movieId, userId);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        
+        return RedirectToAction(nameof(All));
+    }
+
+    
+    public async Task<IActionResult> Watched()
+    {
+        var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value; 
+
+        var model = await movieService.GetWatchedAsync(userId);
+
+        return View("Watched", model);
+    }
+
+    public async Task<IActionResult> RemoveFromCollection(int movieId)
+    {
+        var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        await movieService.RemoveMovieFromCollectionAsync(movieId, userId);
+
+        return RedirectToAction(nameof(Watched));
+    }
+}
